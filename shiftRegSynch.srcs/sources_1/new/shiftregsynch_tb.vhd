bf795c8be847654b49c9a685ba509c7f6a73285d
Cyrill Aebischer <cyril.aebischer@edu.hefr.ch>
2022-07-20 16:01:21 -1000

    BERT implemented

--- shiftRegSynch.srcs/sources_1/new/shiftregsynch_tb.vhd
+++ shiftRegSynch.srcs/sources_1/new/shiftregsynch_tb.vhd
@@ -43,9 +43,18 @@ end shiftregsynch_top;
 
 architecture Behavioral of shiftregsynch_top is
 
-    -- clocking  and TB reset signals
+    -- clocking  and TB signals
     constant clk_i_period : time := 0.78 ns;  -- 1/127 = 7.8740157
     signal clk_i, rst_i, clk_div: std_logic;
+    signal start_test   : std_logic := '0';
+    
+    -- BERT
+    signal en_bert_s   : std_logic;
+    signal d_in_mem    : std_logic;
+    signal data_bert_s : std_logic_vector(0 downto 0);
+    signal addr_bert_s : std_logic_vector(15 downto 0);
+    signal bert_err_cnt_s : std_logic_vector(15 downto 0);
+    signal bert_bit_cnt_s : std_logic_vector(15 downto 0);
     
     signal rst_clkdiv : std_logic;
     
@@ -62,6 +71,10 @@ architecture Behavioral of shiftregsynch_top is
     -- Data Extraction
     signal word_pack_s  : std_logic_vector(31 downto 0);
     signal word_output_s : std_logic;
+    signal valid_s      : std_logic;                                                                        
+    signal wr_en_s      : std_logic;     
+    signal rst_fsm_s    : std_logic;                                                                       
+    
     
     ---------------------------------------------------------------------------------
     -- BEGIN COMPONENT DECLARATIONS --
@@ -103,8 +116,59 @@ architecture Behavioral of shiftregsynch_top is
                 wr_en_o: out std_logic;
                 wr_adr_o : out std_logic_vector(5 downto 0));
        end component;
+       
+       COMPONENT blk_mem_gen_0
+         PORT (
+           clka : IN STD_LOGIC;
+           addra : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+           douta : OUT STD_LOGIC_VECTOR(0 DOWNTO 0)
+         );
+       END COMPONENT;
         
 begin
+
+    ---- BERT ----
+    BERT_mem : blk_mem_gen_0
+        PORT MAP (
+        clka => clk_i,
+        addra => addr_bert_s,
+        douta => data_bert_s
+        );
+    
+    start_bert: process(clk_i)
+        begin
+        if rising_edge(clk_i) then
+            if rst_i = '1' then
+                en_bert_s <= '0';
+            else 
+                if d_in_mem /= bit_in_s then
+                    en_bert_s <= '1';
+                end if;
+            end if;
+        end if;
+        end process;
+        
+    BERT: process(clk_i, rst_i)
+        variable bert_err_cnt :unsigned(15 downto 0) := (others => '0');
+        variable bert_bit_cnt : unsigned(15 downto 0) := (others => '0');
+        begin
+            if rising_edge(clk_i) then
+                d_in_mem <= bit_in_s;
+                if rst_i = '1' then 
+                    bert_err_cnt := (others => '0');
+                else
+                    if en_bert_s = '1' then 
+                        bert_bit_cnt := bert_bit_cnt + 1; 
+                        if data_bert_s(0) /=  d_in_mem then   
+                            bert_err_cnt := bert_err_cnt + 1;
+                        end if;
+                    end if;
+                end if;
+            end if;
+            bert_err_cnt_s <= std_logic_vector(bert_err_cnt);
+            bert_bit_cnt_s <= std_logic_vector(bert_bit_cnt);
+        end process;
+    addr_bert_s    <= bert_bit_cnt_s;
     
     ---- shiftregister ----
     shift_reg: sipo
@@ -148,38 +212,46 @@ begin
     ---- Bit read in process ----
     rd_values: process(clk_i)
         
-        file fp_output : text is in "C:\Users\Cyrill\Documents\S6\BA-GULFstream\Gulf_Eval_Setup\Gulf_Eval_Setup\8chan_output_err.dat";
+        file fp_output : text is in "C:\Users\Cyrill\Documents\S6\BA-GULFstream\shiftRegSynch_v2\8chan_output.dat";
         variable ln_r     : line;
         variable x : std_logic;
         
         variable stop          : boolean := false;
-        begin   
-            if (rising_Edge(clk_i))then
-                if stop = false then
-                            readline(fp_output,ln_r);
-                            read(ln_r,x);
-                            bit_in_s <= x;
-                            if endfile(fp_output) = true then
-                                stop := true;    
-                            end if;
+        begin 
+            if start_test = '1' then  
+                if (rising_Edge(clk_i))then
+                    if stop = false then
+                                readline(fp_output,ln_r);
+                                read(ln_r,x);
+                                bit_in_s <= x;
+                                if endfile(fp_output) = true then
+                                    stop := true;    
+                                end if;
+                    end if;
+                else
+                    bit_in_s <= bit_in_s;
                 end if;
-            else
-                bit_in_s <= bit_in_s;
             end if;
+    end process;
+    
+start_proc: process
+    begin
+      wait for 40 ns;
+      start_test <= '1';
+      wait;
     end process;
     
     ---- word packaging process ----
 word_pack: process(clk_i, clk_div)
     variable word_cnt_v : unsigned(1 downto 0) := "00";
     begin
-            
-                if rising_edge(clk_div) then 
+                 if rising_edge(clk_div) then 
                     if (comma_s = '0') and (synched_s = '1') then                   
                         case word_cnt_v is
                             when "00" =>
                                 word_pack_s(31 downto 24) <= data8b_s;
                                 word_cnt_v := word_cnt_v +1;
-                                
+                                word_output_s <= '0';
                             when "01" =>
                                 word_pack_s(23 downto 16) <= data8b_s; 
                                 word_cnt_v := word_cnt_v +1;
@@ -195,6 +267,7 @@ word_pack: process(clk_i, clk_div)
                             
                         end case;
                     else
+                        word_cnt_v := "00";                                     --<-
                         word_output_s <= '0';
                         word_pack_s <= (others => '0');
                     end if;
@@ -203,6 +276,10 @@ word_pack: process(clk_i, clk_div)
     end process;
     
     ---- Write to Memory FSM ----
+    valid_s <= word_output_s and (not rxCodeErr);        
+    -- rst_fsm_s <= '1' when (rst_i = '1' or   rxCodeErr = '1' ) else '0';                                          
+    wr_en_out <= wr_en_s when (synched_s = '1') else '0';                                              
+    
     adr_fsm: data_wr_fsm
                Port map (
                    rst_i    => rst_i,
@@ -210,7 +287,7 @@ word_pack: process(clk_i, clk_div)
                    valid_i  => word_output_s,
                    wr_data_in => word_pack_s,
                    wr_data_out => data_wr_out,
-                   wr_en_o  => wr_en_out,
+                   wr_en_o  => wr_en_s,
                    wr_adr_o => addr_wr_out
                    );                   
         
